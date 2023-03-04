a3a7e14352b61dd837e6439c5bc4b6b97cd0ed74
Sîngeorzan Dan <singeorzandan@Singeorzans-MacBook-Air.local>
2023-03-04 16:15:59 +0200

    Finishing the answers of the questions

--- JS/Quiz.js
+++ JS/Quiz.js
@@ -2,3 +2,37 @@ function showItems () {
     document.getElementById("ShowItems").classList.remove("hidden");
     
 }
+
+// function Test() {
+
+//     let spanac = ["Capsuna", "Banana", "Mere"];
+    
+//     for (let i = 0; i < 3; i++) {
+//         console.log(spanac[i])
+//         if (spanac[i] === "Banana") {
+//              spanac = ["Ananas"];
+//         }
+//         else {
+//             spanac = "Jitura";
+//         }
+        
+//     } 
+//    console.log(spanac);
+   
+// }
+// Test();
+
+function Another() {
+    let branza = [{ 'name': 'Dan' }, { 'name': 'Adrian' }];
+    for (let i = 0; i < 2; i++) {
+        if (branza[i].name === 'Dan') {
+            console.log(branza[i].name);
+            
+        }
+    }
+    
+}
+
+Another();
+
+
